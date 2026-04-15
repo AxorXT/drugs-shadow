5fe0b09e62ab1b9f5ca15e7d135e7fd48592aba3
AxorXT <angel.sanchez@uniat.edu.mx>
2026-04-15 08:28:24 -0600

    FirstPersonController

---
+++ Assets/EasyPeasyFirstPersonController/InputManager.cs
@@ -0,0 +1,48 @@
+using EasyPeasyFirstPersonController;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour, IInputManager
+{
+    private PlayerInputActions inputActions;
+
+    public Vector2 moveInput { get; private set; }
+    public Vector2 lookInput { get; private set; }
+    public bool jump { get; private set; }
+    public bool sprint { get; private set; }
+    public bool crouch { get; private set; }
+    public bool slide { get; private set; }
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+
+        inputActions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        inputActions.Player.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Look.canceled += ctx => lookInput = Vector2.zero;
+
+        inputActions.Player.Jump.performed += ctx => jump = true;
+        inputActions.Player.Jump.canceled += ctx => jump = false;
+
+        inputActions.Player.Sprint.performed += ctx => sprint = true;
+        inputActions.Player.Sprint.canceled += ctx => sprint = false;
+
+        inputActions.Player.Crouch.performed += ctx => crouch = true;
+        inputActions.Player.Crouch.canceled += ctx => crouch = false;
+
+        inputActions.Player.Slide.performed += ctx => slide = true;
+        inputActions.Player.Slide.canceled += ctx => slide = false;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+}
